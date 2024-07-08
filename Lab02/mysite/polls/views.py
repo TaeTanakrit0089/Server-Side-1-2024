a7e337b988789260ded7b5725e5a6c5f92f56f11
TaeTanakrit0089 <65070089@kmitl.ac.th>
2024-07-08 16:29:33 +0700

    Added Question Query

--- Lab02/mysite/polls/views.py
+++ Lab02/mysite/polls/views.py
@@ -1,6 +1,21 @@
-# polls/views.py
 from django.http import HttpResponse
+
+from polls.models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("This is the index page of polls app")
+
+
+def detail(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    return HttpResponse("You're looking at question %s." % question.question_text)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
