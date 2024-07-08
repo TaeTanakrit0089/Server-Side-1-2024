bbcd792105be2268125eb6fd613d781a312eb947
TaeTanakrit0089 <65070089@kmitl.ac.th>
2024-07-08 16:51:59 +0700

    Added Question Query

--- Lab02/mysite/polls/views.py
+++ Lab02/mysite/polls/views.py
@@ -1,15 +1,22 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
 
 from polls.models import Question
 
 
 def index(request):
-    return HttpResponse("This is the index page of polls app")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "index.html", context)
+    # return HttpResponse("This is the index page of polls app")
 
 
 def detail(request, question_id):
-    question = Question.objects.get(pk=question_id)
-    return HttpResponse("You're looking at question %s." % question.question_text)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "detail.html", {"question": question})
 
 
 def results(request, question_id):
