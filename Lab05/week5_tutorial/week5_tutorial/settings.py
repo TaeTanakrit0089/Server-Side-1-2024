82faded4bbe9946126fbe36095215df001de0d3b
Tae <65070089@kmitl.ac.th>
2024-08-01 09:56:04 +0700

    Added Books

--- Lab05/week5_tutorial/week5_tutorial/settings.py
+++ Lab05/week5_tutorial/week5_tutorial/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'companies'
+
+    "django_extensions",
+    'companies',
+    'books'
 ]
 
 MIDDLEWARE = [
