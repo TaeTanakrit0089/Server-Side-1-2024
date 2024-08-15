da108a671feb31e675164c060f86df81ade2cb33
Tae <65070089@kmitl.ac.th>
2024-08-15 11:08:17 +0700

    Complete Num 1, 2

--- Lab07-Exercise/employee_management/employee/views.py
+++ Lab07-Exercise/employee_management/employee/views.py
@@ -1,3 +1,24 @@
+from django.db.models import Count
 from django.shortcuts import render
 
-# Create your views here.
+from .models import *
+
+
+def employee(request):
+    employees = Employee.objects.all()
+
+    context = {
+        "employees": employees,
+        "total_employees": len(employees)
+    }
+    return render(request, "employee.html", context)
+
+
+def position(request):
+    positions = Position.objects.annotate(num_employees=Count('employee'))
+
+    context = {
+        "positions": positions,
+    }
+
+    return render(request, "position.html", context)
--- Lab07-Exercise/employee_management/employee_management/settings.py
+++ Lab07-Exercise/employee_management/employee_management/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-27c36!o^b(!&a^gim*=^stp)6*95-f8ixym3-9vuh$_z@-y+dz
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'employee_management.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -84,7 +81,6 @@ DATABASES = {
         "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -104,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -116,7 +111,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
@@ -126,3 +120,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
+
+TEMPLATE_DIRS = (
+    os.path.join(SETTINGS_PATH, 'templates'),
+)
