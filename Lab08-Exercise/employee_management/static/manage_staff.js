ab84d188019146c2f2f0508ff71823d19cbf7131
Tae <65070089@kmitl.ac.th>
2024-08-19 15:26:46 +0700

    Completed Lab

--- Lab08-Exercise/employee_management/static/manage_staff.js
+++ Lab08-Exercise/employee_management/static/manage_staff.js
@@ -1,32 +1,38 @@
-function addStaff(pro_id, staff_id, csrf_token){
+function addStaff(emp, url, token) {
+    const data = {emp_id: emp.value};
+
     // กำหนด path ให้ถูกต้อง
-    fetch(``, {
+    fetch(url, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
-            'X-CSRFToken': csrf_token
+            'X-CSRFToken': token
         },
+        body: JSON.stringify(data)
     })
-    .then(data => {
-        console.log('Item updated successfully')
-        window.location.reload()
-    })
-    .catch(error => console.error('Error:', error));
+        .then(response => response.json())
+        .then(data => {
+            console.log('Item updated successfully')
+            window.location.reload()
+        })
+        .catch(error => console.error('Error:', error));
 }
 
-
-async function removeStaff(pro_id, emp_id, csrf_token){
+async function removeStaff(emp_Id, url, token) {
+    const data = {emp_id: emp_Id};
     // กำหนด path ให้ถูกต้อง
-    fetch(``, {
-        method: 'DELETE',
+    fetch(url, {
+        method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
-            'X-CSRFToken': csrf_token
+            'X-CSRFToken': token
         },
+        body: JSON.stringify(data)
     })
-    .then(data => {
-        console.log('Item updated successfully')
-        window.location.reload()
-    })
-    .catch(error => console.error('Error:', error));
+        .then(response => response.json())
+        .then(data => {
+            console.log('Item updated successfully')
+            window.location.reload()
+        })
+        .catch(error => console.error('Error:', error));
 }
--- Lab08-Exercise/employee_management/static/project.js
+++ Lab08-Exercise/employee_management/static/project.js
@@ -1,16 +1,17 @@
-
-function deleteProject(pro_id, csrf_token) {
-    // กำหนด path ให้ถูกต้อง
-    fetch(``, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        "X-CSRFToken": csrf_token,
-      },
+function deleteProject(pro_id, url, token) {
+    const data = {project_id: pro_id};
+    fetch(url, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': token
+        },
+        body: JSON.stringify(data)
     })
-      .then((data) => {
-        console.log("Item deleted successfully");
-        window.location.reload();
-      })
-      .catch((error) => console.error("Error:", error));
-  }
+        .then(response => response.json())
+        .then(data => {
+            console.log('Item deleted successfully')
+            window.location.reload()
+        })
+        .catch(error => console.error('Error:', error));
+}
