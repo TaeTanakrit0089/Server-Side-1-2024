f292abe1aeb50596c16bdd4a007be31ddb5e1a34
Tae <65070089@kmitl.ac.th>
2024-09-23 14:17:15 +0700

    Added Week 12 Tutorial

--- Lab12/week12_tutorial/bookings/views.py
+++ Lab12/week12_tutorial/bookings/views.py
@@ -1,3 +1,103 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
+from django.shortcuts import render, redirect
+from django.views import View
+
+from bookings.forms import BookingForm
+from bookings.models import Booking
+
+
+class Login(View):
+    def get(self, request):
+        form = AuthenticationForm()
+        return render(request, 'login.html', {"form": form})
+
+    def post(self, request):
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('booking-list')
+
+        return render(request, 'login.html', {"form": form})
+
+
+class Logout(View):
+    def get(self, request):
+        logout(request)
+        return redirect('login')
+
+
+class BookingList(LoginRequiredMixin, View):
+    login_url = '/login/'
+
+    def get(self, request):
+        query = request.GET
+        bookings = Booking.objects.order_by("start_time")
+
+        if query.get("search"):
+            bookings = bookings.filter(
+                Q(room__name__icontains=query.get("search")) |
+                Q(staff__name__icontains=query.get("search"))
+            )
+
+        return render(request, "booking-list.html", {
+            "bookings": bookings
+        })
+
+
+class BookingCreate(LoginRequiredMixin, View):
+    login_url = '/login/'
+
+    def get(self, request):
+        form = BookingForm()
+        return render(request, "booking.html", {
+            "form": form,
+        })
+
+    def post(self, request):
+        form = BookingForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('booking-list')
+
+        return render(request, "booking.html", {
+            "form": form
+        })
+
+
+class BookingUpdate(LoginRequiredMixin, View):
+    login_url = '/login/'
+
+    def get(self, request, booking_id):
+        booking = Booking.objects.get(pk=booking_id)
+        form = BookingForm(instance=booking)
+        return render(request, "booking.html", {
+            "form": form,
+        })
+
+    def post(self, request, booking_id):
+        booking = Booking.objects.get(pk=booking_id)
+        form = BookingForm(request.POST, instance=booking)
+
+        if form.is_valid():
+            form.save()
+            return redirect('booking-list')
+
+        return render(request, "booking.html", {
+            "form": form
+        })
+
+
+class BookingDelete(LoginRequiredMixin, View):
+    login_url = '/login/'
+
+    def get(self, request, booking_id):
+        booking = Booking.objects.get(pk=booking_id)
+        booking.delete()
+
+        return redirect("booking-list")
--- Lab12/week12_tutorial/week12_tutorial/urls.py
+++ Lab12/week12_tutorial/week12_tutorial/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+# week12_tutorial/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from bookings.views import Login, Logout
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("bookings/", include("bookings.urls")),
+    path("login/", Login.as_view(), name="login"),
+    path("logout/", Logout.as_view(), name="logout"),
+
 ]
