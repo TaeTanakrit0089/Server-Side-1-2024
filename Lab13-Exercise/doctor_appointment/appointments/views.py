ca1388f7c0b65d3a085ea67749bcc4ff5e12f1a1
Tae <65070089@kmitl.ac.th>
2024-09-30 15:16:50 +0700

    Complete HW 13.01

---
+++ Lab13-Exercise/doctor_appointment/appointments/views.py
@@ -0,0 +1,27 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from appointments.models import Doctor, Patient, Appointment
+from appointments.serializers import DoctorSerializer, PatientSerializer, AppointmentSerializer
+
+
+# Create your views here.
+class DoctorList(APIView):
+    def get(self, request):
+        doctors = Doctor.objects.all()
+        serializer = DoctorSerializer(doctors, many=True)
+        return Response(serializer.data)
+
+
+class PatientList(APIView):
+    def get(self, request):
+        patients = Patient.objects.all()
+        serializer = PatientSerializer(patients, many=True)
+        return Response(serializer.data)
+
+
+class AppointmentList(APIView):
+    def get(self, request):
+        appointments = Appointment.objects.all()
+        serializer = AppointmentSerializer(appointments, many=True)
+        return Response(serializer.data)
